e7663f2bb2337cbb9368f2b30ca8866e81b1dee3
Sanjaya_023 <sanjaya20004@gmail.com>
2024-08-14 19:14:48 +0700

    Latihan Request pakai Promise hal.1-25

--- Latihan 2 Promise/script.js
+++ Latihan 2 Promise/script.js
@@ -2,11 +2,11 @@
 const requestCallback = (url, success, failure) => {
     const delay = Math.floor(Math.random() * 4500) + 500;
     setTimeout(() => {
-        if (delay > 4000) {
-        failure("Error: Connection Timeout");
-        } else {
-        success(`Success: ${url} (${delay}ms)`);
-        }
+            if (delay > 4000) {
+                failure("Error: Connection Timeout");
+            } else {
+                success(`Success: ${url} (${delay}ms)`);
+            }
     }, delay);
 };
 
@@ -15,58 +15,58 @@ const requestPromise = (url) => {
     return new Promise((resolve, reject) => {
         const delay = Math.floor(Math.random() * 4500) + 500;
         setTimeout(() => {
-        if (delay > 2000) {
-            reject("Error: Connection Timeout");
-        } else {
-            resolve(`Success: ${url} (${delay}ms)`);
-        }
+            if (delay > 2000) {
+                reject("Error: Connection Timeout");
+            } else {
+                resolve(`Success: ${url} (${delay}ms)`);
+            }
         }, delay);
     });
 };
 
 // Latihan Request pakai Callback function
-requestCallback(
-	'movie.com',
-	function (response) {
-		console.log('success', response);
-		requestCallback(
-			'movie.com',
-			function (response) {
-				console.log('success', response);
-				requestCallback(
-					'movie.com',
-					function (response) {
-						console.log('success', response);
-						requestCallback(
-							'movie.com',
-							function (response) {
-								console.log('success', response);
-								requestCallback(
-									'movie.com',
-									function (response) {
-										console.log('success', response);
-									},
-									function (error) {
-										console.log('error', error);
-									}
-								);
-							},
-							function (error) {
-								console.log('error', error);
-							}
-						);
-					},
-					function (error) {
-						console.log('error', error);
-					}
-				);
-			},
-			function (error) {
-				console.log('error', error);
-			}
-		);
-	},
-	function (error) {
-		console.log('error', error);
-	}
-);
+// requestCallback(
+// 	'movie.com',
+// 	function (response) {
+// 		console.log('success', response);
+// 		requestCallback(
+// 			'movie.com',
+// 			function (response) {
+// 				console.log('success', response);
+// 				requestCallback(
+// 					'movie.com',
+// 					function (response) {
+// 						console.log('success', response);
+// 						requestCallback(
+// 							'movie.com',
+// 							function (response) {
+// 								console.log('success', response);
+// 								requestCallback(
+// 									'movie.com',
+// 									function (response) {
+// 										console.log('success', response);
+// 									},
+// 									function (error) {
+// 										console.log('error', error);
+// 									}
+// 								);
+// 							},
+// 							function (error) {
+// 								console.log('error', error);
+// 							}
+// 						);
+// 					},
+// 					function (error) {
+// 						console.log('error', error);
+// 					}
+// 				);
+// 			},
+// 			function (error) {
+// 				console.log('error', error);
+// 			}
+// 		);
+// 	},
+// 	function (error) {
+// 		console.log('error', error);
+// 	}
+// );
